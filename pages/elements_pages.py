94e6433e6a1ae3a2013d9066a4289cced1e21f3d
okun91 <r.zinkevich1991@gmail.com>
2023-05-11 11:05:35 +0300

    Add class RadioButtonPage

--- pages/elements_pages.py
+++ pages/elements_pages.py
@@ -1,4 +1,4 @@
-from locators.elements_page_locators import TextBoxPageLocators, CheckBoxPageLocators
+from locators.elements_page_locators import TextBoxPageLocators, CheckBoxPageLocators, RadioButtonPageLocators
 from pages.base_page import BasePage
 from generator.generator import generator_person
 import random
@@ -57,3 +57,19 @@ class CheckBoxPage(BasePage):
         for i in result_list:
             data.append(i.text)
         return str(data).replace(' ', '').lower()
+
+
+class RadioButtonPage(BasePage):
+    locators = RadioButtonPageLocators()
+
+    def click_radio_button(self, choice):
+        choices = {
+            'yes': self.locators.RADIOBUTTON_YES,
+            'impressive': self.locators.RADIOBUTTON_IMPRESSIVE,
+            'no': self.locators.RADIOBUTTON_NO
+        }
+        self.element_is_visible(choices[choice]).click()
+
+    def get_output_result(self):
+        return self.element_is_present(self.locators.OUTPUT_RESULT).text
+
