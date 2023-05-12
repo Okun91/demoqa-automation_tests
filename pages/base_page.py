dc69c9477ff4fd715ce09b2e49713d89607a94be
okun91 <r.zinkevich1991@gmail.com>
2023-05-12 21:18:40 +0300

    Add: action_double_click, action_right_click

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,3 +1,4 @@
+from selenium.webdriver import ActionChains
 from selenium.webdriver.support.ui import WebDriverWait as wait
 from selenium.webdriver.support import expected_conditions as EC
 
@@ -34,7 +35,16 @@ class BasePage:
     def scroll(self):
         self.driver.execute_script("window.scrollBy(0, 200);")
 
+    def action_double_click(self, element):
+        action = ActionChains(self.driver)
+        action.double_click(element)
+        action.perform()
+
+    def action_right_click(self, element):
+        action = ActionChains(self.driver)
+        action.context_click(element)
+        action.perform()
+
     def remove_footer(self):
         self.driver.execute_script("document.getElementsByTagName('footer')[0].remove();")
         self.driver.execute_script("document.getElementById('close-fixedban').remove();")
-
