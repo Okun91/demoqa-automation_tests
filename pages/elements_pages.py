26293fc1f5af90a8cde59c64cf959af7e0fd17fc
okun91 <r.zinkevich1991@gmail.com>
2023-06-07 19:17:15 +0300

    Add class DynamicPropertiesPage: def check_enable_button, def check_changed_color, def check_appear_button

--- pages/elements_pages.py
+++ pages/elements_pages.py
@@ -1,15 +1,15 @@
 import base64
-
 import requests
 from selenium.webdriver.common.by import By
-from selenium.common.exceptions import NoSuchElementException, WebDriverException
+from selenium.common.exceptions import NoSuchElementException, WebDriverException, TimeoutException
 from locators.elements_page_locators import TextBoxPageLocators, CheckBoxPageLocators, RadioButtonPageLocators, \
-    WebTablePageLocators, ButtonsPageLocators, LinksPageLocators, UploadAndDownloadPageLocators
+    WebTablePageLocators, ButtonsPageLocators, LinksPageLocators, UploadAndDownloadPageLocators, \
+    DynamicPropertiesPageLocators
 from pages.base_page import BasePage
 from generator.generator import generator_person, generated_file
 import random
 import os
-
+import time
 
 class TextBoxPages(BasePage):
     locators = TextBoxPageLocators()
@@ -212,4 +212,30 @@ class UploadAndDownloadPage(BasePage):
             check_file = os.path.exists(path_name_file)
         os.remove(path_name_file)
         return check_file
+
+
+class DynamicPropertiesPage(BasePage):
+    locators = DynamicPropertiesPageLocators()
+
+    def check_enable_button(self):
+        try:
+            self.element_is_clickable(self.locators.ENABLE_AFTER_FIVE_SEC)
+        except TimeoutException:
+            return "Timeout"
+        return True
+
+    def check_changed_color(self):
+        color_button = self.element_is_present(self.locators.COLOR_CHANGE_BUTTON,)
+        color_button_before = color_button.value_of_css_property('color')
+        time.sleep(5)
+        color_button_after = color_button.value_of_css_property('color')
+        return color_button_before, color_button_after
+
+    def check_appear_button(self):
+        try:
+            self.element_is_visible(self.locators.VISIBLE_AFTER_FIVE_SEC)
+        except TimeoutException:
+            return "Timeout"
+        return True
+
 
